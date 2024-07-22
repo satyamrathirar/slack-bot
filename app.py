5523b96d57cdcba59aafabd1a3c3c906cf7784cb
Satyam Rathi <satyam.rathi47@gmail.com>
2024-07-22 17:17:35 +0530

    started with slash commands

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
@@ -14,14 +14,35 @@ slack_event_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'], '/sl
 client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
+message_counts = {
+    'user_id': 0
+    }
+
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-    if BOT_ID != user_id and 'hello' in text.lower():
+    
+    if BOT_ID != user_id:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+            
         client.chat_postMessage(channel=channel_id, text="Hi <@{}>!".format(user_id))
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id, 0)
+    
+    client.chat_postMessage(channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
